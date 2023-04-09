fa24a775037849f9c89a3627a3c635b3b5462723
Samasth Nayak <samasthnayak1@gmail.com>
2023-04-09 11:24:12 +0530

    uuid added. Messages don't send twice

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState} from 'react'
-
+import uuid from "uuid";
 function Chat({socket,username,room}) {
     const  [currentMessage,setCurrentMessage]=useState('')
     const [messageList,setMessageList]=useState([])
     async function sendMessage(){
         if(!currentMessage!==''){
             const messageData={
+               id:uuid.v4(),
                 room:room,
                 sender:username,
                 message:currentMessage,
@@ -19,8 +20,10 @@ function Chat({socket,username,room}) {
         useEffect(()=>{
            socket.on('receive_message',(data)=>{
                 setMessageList(list=>[...list,data])
+                
                 console.log(data)
            })
+           return () => socket.removeListener('receive_message')
         },[socket])
     
   return (
@@ -31,7 +34,7 @@ function Chat({socket,username,room}) {
      <div className='chat-body'>
          {messageList.map((messageData)=>{
             return( 
-            <div>
+            <div key={messageData.id}>
             <h4>{messageData.sender}</h4>
             <h2>{messageData.message}</h2>
             </div>
